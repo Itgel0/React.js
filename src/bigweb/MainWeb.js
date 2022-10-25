06a1aac88f7621d3ebd1f2b3aadd6308ba03af52
Munkh-Itgel <Itgel@nest-25s-iMac.local>
2022-10-25 19:45:31 +0800

    change

--- src/bigweb/MainWeb.js
+++ src/bigweb/MainWeb.js
@@ -5,7 +5,19 @@ import './MainWeb.css';
 const MainWeb = () => {
     return(
         <div className="webContainer">
-            
+            <div className="top">
+                <div className="t-logo">
+                    <div className="t-text">team</div>
+                    <div className="dot">.</div>
+                </div>
+                <div className="buttonz">
+                    <button className="Products">Products</button>
+                    <button className="Services">Services</button>
+                    <button className="Contact">Contact</button>
+                    <button className="Log-in">Log in</button>
+                    <div className="Get-Access">Get Access</div>
+                </div>
+            </div>
         </div>
     )
 }
