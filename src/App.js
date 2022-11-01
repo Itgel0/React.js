00ccd3279b709b1ccc0d70b1477ef566d93f6d03
Munkh-Itgel <Itgel@nest-25s-iMac.local>
2022-11-01 19:28:49 +0800

    pushtophone

--- src/App.js
+++ src/App.js
@@ -7,38 +7,51 @@ import SignUp from "./bigweb/SignUp";
 import MainWeb from "./bigweb/MainWeb";
 import Teamwork from "./bigweb/Teamwork";
 import Work from "./bigweb/Work";
-
+import proof from "./bigweb/proof";
 import "./App.css";
 
 function App() {
-  const Header = () => {
-    return (
-      <div className="App">
-        <Link to="/Task">Task</Link>
-        <Link to="/Todo">Todo</Link>
-        <Link to="/SignUp">SignUp</Link>
-        <Link to="MainWeb"></Link>
-        <Link to="StpWatch">StpWatch</Link>
-        <Link to="Teamwork">Teamwork</Link>
-      </div>
-    );
-  };
-
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/Task" element={<Task />} />
         <Route path="/StpWatch" element={<StpWatch />} />
         <Route path="SignUp" element={<SignUp />} />
+        <Route path="proof" element={<proof />} />
         {/* <Route path="MainWeb" element={<MainWeb />}></Route>
         <Route path="/Teamwork" element={<Teamwork />} />
-        <Route path="/Work" element={<Work />} /> */}
+        <Route path="/Work" element={<Work />} />
+        <Route path="/proof" element={<proof> />} */}
       </Routes>
-      <MainWeb />
-      <Teamwork />
-      <Work />
     </BrowserRouter>
   );
 }
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <MainWeb />
+      <Teamwork />
+      <Work />
+      
+    </div>
+  );
+};
+
+const Header = () => {
+  return (
+    <div className="App">
+      <Link to="/Task">Task</Link>
+      <Link to="/Todo">Todo</Link>
+      <Link to="/SignUp">SignUp</Link>
+      <Link to="MainWeb">MainWeb</Link>
+      <Link to="StpWatch">StpWatch</Link>
+      <Link to="Teamwork">Teamwork</Link>
+      <Link to="proof">proof</Link>
+    </div>
+  );
+};
 
 export default App;
---
+++ src/bigweb/proof.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
+import "./proof.css";
+
+const proof = () => {
+    return (
+        <div className="container-of-prf">
+            <div className="second-work">
+                <div className="bunchaTitle">
+                    <p className="hugeTitle">Scheduling that actually works</p>
+                    <p className="smallTitle">Give everyone you work with—inside and outside your company—a more productive way to stay in sync. Respond faster with emoji, keep conversations focused in channels, and simplify all your communication into one place.</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+export default proof;
